049132a6bb37284ba2fb8f11f004dc71800e9106
竹内隼人 <takeuchihayato@takeuchihayatonoMacBook-Pro.local>
2017-08-23 22:42:20 +0900

    after recipe_process migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823132324) do
+ActiveRecord::Schema.define(version: 20170823133203) do
+
+  create_table "recipe_processes", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
