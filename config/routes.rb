b66a0c28013ce80e98a653196c89dcb621b589af
竹内隼人 <takeuchihayato@takeuchihayatonoMacBook-Pro.local>
2017-08-25 23:20:34 +0900

    change top route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'top/index'
+  root to: 'top#index'
+
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
