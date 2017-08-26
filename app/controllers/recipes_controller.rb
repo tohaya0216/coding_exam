169334102dbded7792e534ffaaf2d910f7cdc2cf
竹内隼人 <takeuchihayato@takeuchihayatonoMacBook-Pro.local>
2017-08-26 21:27:05 +0900

    文言日本語化

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
+  MAX_MATERIAL_NUM = 5
+  MAX_PROCESS_NUM = 5
   # GET /recipes
   # GET /recipes.json
   def index
@@ -15,14 +17,14 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
-#    @recipe.recipe_processes.build
-#    @recipe.materials.build
-    5.times { @recipe.recipe_processes.build }
-    5.times { @recipe.materials.build }
+    MAX_PROCESS_NUM.times { @recipe.recipe_processes.build }
+    MAX_MATERIAL_NUM.times { @recipe.materials.build }
   end
 
   # GET /recipes/1/edit
   def edit
+    (MAX_PROCESS_NUM-@recipe.recipe_processes.length).times { @recipe.recipe_processes.build }
+    (MAX_MATERIAL_NUM-@recipe.materials.length).times { @recipe.materials.build }
   end
 
   # POST /recipes
@@ -32,7 +34,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
+        format.html { redirect_to @recipe, notice: 'レシピの投稿が完了しました' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new }
@@ -44,11 +46,9 @@ class RecipesController < ApplicationController
   # PATCH/PUT /recipes/1
   # PATCH/PUT /recipes/1.json
   def update
-    puts "======"
-    puts params
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
+        format.html { redirect_to @recipe, notice: 'レシピを更新しました' }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit }
@@ -62,7 +62,7 @@ class RecipesController < ApplicationController
   def destroy
     @recipe.destroy
     respond_to do |format|
-      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+      format.html { redirect_to recipes_url, notice: 'レシピは削除されました' }
       format.json { head :no_content }
     end
   end
