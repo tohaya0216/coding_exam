28ac120f4f5a03c075b9fff4acfac1525eeea70f
竹内隼人 <takeuchihayato@takeuchihayatonoMacBook-Pro.local>
2017-08-26 21:17:22 +0900

    日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module CodingExam
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    I18n.available_locales = [:en, :ja]
+    I18n.enforce_available_locales = true
+    I18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
