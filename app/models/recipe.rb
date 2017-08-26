af9bb858574cdb980eef33a5720c360bd35f58e4
竹内隼人 <takeuchihayato@takeuchihayatonoMacBook-Pro.local>
2017-08-26 21:24:22 +0900

    validation,relation追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,26 @@
 class Recipe < ApplicationRecord
-   has_many :recipe_processes, dependent: :destroy
-   accepts_nested_attributes_for :recipe_processes
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :expense, numericality: true
+  validates :time, numericality: true
 
-   has_many :materials, dependent: :destroy
-   accepts_nested_attributes_for :materials
+  has_many :recipe_processes, dependent: :destroy
+  accepts_nested_attributes_for :recipe_processes
 
-  has_many :recipe_tag
-  has_many :tag, through: :recipe_tag
+  has_many :materials, dependent: :destroy
+  accepts_nested_attributes_for :materials
+
+  has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
 
   # photoをattachファイルとする。stylesで画像サイズを定義できる
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  #has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
 
-  # ファイルの拡張子を指定（これがないとエラーが発生する）
-  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  has_attached_file :photo,
+                  :storage => :s3,
+                  :s3_permissions => :"public-read",
+                  :path => ":attachment/:id/:style.:extension"
 
+  validates_attachment :photo, content_type: { content_type: ["movie/mp4","video/mp4","image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 end
